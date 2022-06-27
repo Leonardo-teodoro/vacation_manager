dea7e2d423e35e6a3f87c6e6eb761c192dddbb1a
leonardo-Teodoro <leonardo.teodoro@aluno.cefet-rj.br>
2022-06-27 04:04:46 -0300

    Add employees validations and defaults

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,18 @@
 class Employee < ApplicationRecord
+  has_many :vacation_ranges, dependent: :destroy
+  after_initialize :init_defaults
+  validates :name, presence: {message: "O nome deve ser preenchido."}
+  validates :role, presence: {message: "O cargo deve ser preenchido."}
+
+  def init_defaults
+    self.vacation_days ||=0
+    self.active ||=true
+    self.hired_in ||=DateTime.current
+  end
+
+  def vacation_eligible?
+    (Date.today - hired_in).to_i >= 365 && vacation_days >= 10
+  end
+
+
 end
