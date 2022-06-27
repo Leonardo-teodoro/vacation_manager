f82638d241cf5eefc873b49af7a0a9176c0f7715
leonardo-Teodoro <leonardo.teodoro@aluno.cefet-rj.br>
2022-06-27 04:04:06 -0300

    Add helper methods to format date and use icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def raw_icon(name, content)
+    raw("<i class=\"bi bi-" + name+ "\">  "+content+"</i>")
+  end
+  
+  def date_in_Ptbr(date)
+    I18n.l(date, format: :long)
+  end
 end
