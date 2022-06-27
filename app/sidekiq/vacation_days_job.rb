01236c0d1b970efe3cfd58698a5fc7028cf6c0d3
leonardo-Teodoro <leonardo.teodoro@aluno.cefet-rj.br>
2022-06-27 04:31:29 -0300

    Fix vacation days job logic

--- app/sidekiq/vacation_days_job.rb
+++ app/sidekiq/vacation_days_job.rb
@@ -5,7 +5,7 @@ class VacationDaysJob
     puts "Pesquisando todos os funcionários..."
     employees = Employee.all
     employees.each do |employee|
-      if Date.today.years_ago(1) == employee.hired_in
+      if (Date.today - employee.hired_in).to_i % 365 == 0
         employee.update(vacation_days: employee.vacation_days + 30)
         puts "Funcionário "+ employee.name + " recebeu +30 dias disponíveis para férias!"
       end
